d5908290d1d888796f1880592d6a6d5cf8da8c2e
Anni <anni.simsalu@vikk.ee>
2025-03-03 20:45:38 +0200

    lisatud 2 funktsiooni ja kommentaarid

--- calc.py
+++ calc.py
@@ -1,9 +1,11 @@
-import math
-class cal():
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
+import math #impordib math funktsioonid
 
+class cal():   #loob classi nimega cal
+    def __init__(self,a,b):   #loob muutujad a ja b
+        self.a = a   #nimetab a nimeks self.a
+        self.b = b   #nimetab b nimeks self.b
+
+    # Määrab funktsioonid näiteks muutujate liitmine ja lahutamine
     def liitmine(self):
         return self.a + self.b
     def lahutamine(self):
@@ -20,19 +22,21 @@ class cal():
         return self.a ** self.b
     def protsent(self):
         return self.a / self.b * 100
-    def
-        return
+    def protsendi_liitmine(self):
+        return self.a + (self.b * self.a / 100)
 
-a = int(input("Sisesta esimene number: "))
-b = int(input("Sisesta teine number: "))
+a = int(input("Sisesta esimene number: "))   #küsib kasutajalt muutujat a
+b = int(input("Sisesta teine number: "))   #küsib kasutajalt muutujat b
 
-kalk = cal(a,b)
-while True:
-    def menu():
-        x = ('1. Liitmine \n2. lahutamine\n3. korrutamine\n4. jagamine\n5. Jäägi leidmine\n6. Ruutjuure leidmine\n7. Astendamine\n8. Protsent\n9.')
+kalk = cal(a,b)  #annab cal() klassile ja sinna sisse kuuluvatele muutujatele a ja b nime kalk
+while True:  #skript toimib kuni tuleb break käsk
+    def menu():  #defineerib menüü
+        # annab tekstile nime x ja prindib selle
+        x = ('1. Liitmine \n2. lahutamine\n3. korrutamine\n4. jagamine\n5. Jäägi leidmine\n6. Ruutjuure leidmine\n7. Astendamine\n8. Protsent\n9. Protsent liitmine')
         print(x)
     menu()
-    valik = int(input('Sisesta üks valikutest: '))
+    valik = int(input('Sisesta üks valikutest: '))  #küsib kasutajalt millist funktsiooni kasutatakse
+    # käivitavad vastavalt valitud funktsioonile üleval tehtud classi funktsiooni
     if valik == 1:
         print("Vastus: ",kalk.liitmine())
         break
@@ -58,8 +62,9 @@ while True:
         print("Vastus:",kalk.protsent())
         break
     elif valik == 9:
-        print("Vastus: ",kalk.)
-    else:
+        print("Vastus: ",kalk.protsendi_liitmine())
+        break
+    else:  #kui sisestati number mis ei ole valikus siis prinditakse all määratud tekst
         print('Sisesta uuesti üks liitmise operaator')
         break
-#15:15
+#20:07
